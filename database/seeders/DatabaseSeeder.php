3810d400f3bb7d258cda38c3ecbdbf8cacda11fc
rerisone <rerisone@gmail.com>
2023-07-07 17:09:18 +0700

    9. Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Posts;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +18,59 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name'=>'Reris Pratama',
+            'email'=>'rerisone@gmail.com',
+            'password'=>bcrypt('12345'),
+        ]);
+        User::create([
+            'name'=>'Gawr Gura',
+            'email'=>'gawr@gmail.com',
+            'password'=>bcrypt('12345'),
+        ]);
+
+        Category::create([
+            'name'=>'Web Programming',
+            'slug'=>'web-programming',
+        ]);
+        Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal',
+        ]);
+
+        Post::create([
+            'title'=>'Judul Pertama',
+            'slug'=>'judul-pertama',
+            'excerpt'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam',
+            'body'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam, rem id suscipit inventore totam. Quia, voluptates et distinctio iste perferendis eos nihil excepturi, voluptatem quibusdam culpa non officiis totam? Accusantium, fugiat, veniam maiores doloribus velit error totam numquam voluptates enim saepe tenetur dicta? Eum cum, quidem assumenda nostrum itaque debitis, maxime cupiditate eius distinctio repellat ad praesentium. Labore veniam doloribus quaerat, exercitationem sunt nisi nobis dolorem odit quam necessitatibus sint voluptatem nesciunt dolorum adipisci iste similique aut.',
+            'category_id'=>1,
+            'user_id'=>1
+        ]);
+
+        Post::create([
+            'title'=>'Judul Kedua',
+            'slug'=>'judul-kedua',
+            'excerpt'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam',
+            'body'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam, rem id suscipit inventore totam. Quia, voluptates et distinctio iste perferendis eos nihil excepturi, voluptatem quibusdam culpa non officiis totam? Accusantium, fugiat, veniam maiores doloribus velit error totam numquam voluptates enim saepe tenetur dicta? Eum cum, quidem assumenda nostrum itaque debitis, maxime cupiditate eius distinctio repellat ad praesentium. Labore veniam doloribus quaerat, exercitationem sunt nisi nobis dolorem odit quam necessitatibus sint voluptatem nesciunt dolorum adipisci iste similique aut.',
+            'category_id'=>1,
+            'user_id'=>1
+        ]);
+        Post::create([
+            'title'=>'Judul Ketiga',
+            'slug'=>'judul-ketiga',
+            'excerpt'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam',
+            'body'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam, rem id suscipit inventore totam. Quia, voluptates et distinctio iste perferendis eos nihil excepturi, voluptatem quibusdam culpa non officiis totam? Accusantium, fugiat, veniam maiores doloribus velit error totam numquam voluptates enim saepe tenetur dicta? Eum cum, quidem assumenda nostrum itaque debitis, maxime cupiditate eius distinctio repellat ad praesentium. Labore veniam doloribus quaerat, exercitationem sunt nisi nobis dolorem odit quam necessitatibus sint voluptatem nesciunt dolorum adipisci iste similique aut.',
+            'category_id'=>2,
+            'user_id'=>1
+        ]);
+        Post::create([
+            'title'=>'Judul Kempat',
+            'slug'=>'judul-keempat',
+            'excerpt'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam',
+            'body'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam, rem id suscipit inventore totam. Quia, voluptates et distinctio iste perferendis eos nihil excepturi, voluptatem quibusdam culpa non officiis totam? Accusantium, fugiat, veniam maiores doloribus velit error totam numquam voluptates enim saepe tenetur dicta? Eum cum, quidem assumenda nostrum itaque debitis, maxime cupiditate eius distinctio repellat ad praesentium. Labore veniam doloribus quaerat, exercitationem sunt nisi nobis dolorem odit quam necessitatibus sint voluptatem nesciunt dolorum adipisci iste similique aut.',
+            'category_id'=>2,
+            'user_id'=>2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article>
         <h2>{{ $post->title }}</h2>
         
-        <p>By . Reris in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By . <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
     </article>
@@ -18,3 +18,5 @@
     'excerpt' => 'Lorem ipsum ketiga',
     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda adipisci quaerat accusamus quisquam porro, culpa, libero cum illo sequi nostrum architecto est voluptates sunt quis? Ullam eos magni libero ex voluptatum.</p><p>Veniam accusamus mollitia autem cumque provident nesciunt at. Hic repudiandae sed rerum! Et obcaecati nulla reiciendis ab praesentium, ut numquam quidem? Quod porro voluptatibus velit numquam! Consequatur eveniet eum quis commodi vero rem nesciunt perspiciatis minima alias ullam, quas aliquam ipsum, repudiandae recusandae, maxime eos error unde.</p><p>Sunt porro facere similique nam illo pariatur alias velit sed, quas sequi consectetur quae mollitia eaque, magni a error aut illum veritatis fuga repellendus reprehenderit tempore possimus et.</p><p>Aperiam ipsa necessitatibus praesentium earum dolorem labore maiores, possimus dicta aliquam, quo esse accusantium porro repellendus dolorum doloremque veniam impedit fuga perferendis? Aut, maiores adipisci corrupti delectus neque voluptatem excepturi quidem libero, inventore sapiente nobis consectetur obcaecati blanditiis tenetur, debitis qui hic nihil deserunt?</p>'
 ]) --}}
+
+{{-- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde saepe dicta dignissimos accusamus dolor nisi voluptatem maiores exercitationem iure, quaerat possimus qui reprehenderit amet eligendi tenetur ducimus corporis voluptatum beatae sapiente veniam, rem id suscipit inventore totam. Quia, voluptates et distinctio iste perferendis eos nihil excepturi, voluptatem quibusdam culpa non officiis totam? Accusantium, fugiat, veniam maiores doloribus velit error totam numquam voluptates enim saepe tenetur dicta? Eum cum, quidem assumenda nostrum itaque debitis, maxime cupiditate eius distinctio repellat ad praesentium. Labore veniam doloribus quaerat, exercitationem sunt nisi nobis dolorem odit quam necessitatibus sint voluptatem nesciunt dolorum adipisci iste similique aut. --}}
