7a00e8854fc12dd51bd0443a8ff81027d0fb08a3
rerisone <rerisone@gmail.com>
2023-07-07 13:59:40 +0700

    8. Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,9 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        {{-- <h5>{{ $post->author }}</h5> --}}
+        
+        <p>By . Reris in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
         {!! $post->body !!}
     </article>
 
