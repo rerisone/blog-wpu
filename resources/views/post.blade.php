4f49fb897f0fd4863452cfee39f5ffa7591340cf
rerisone <rerisone@gmail.com>
2023-07-10 22:30:37 +0700

    12. Redesign UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title }}</h2>
-        
-        <p>By . <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h1 class="mb-3">{{ $post->title }}</h1>
+                <p>By . <a href="/authors/{{ $post->author->username }}"
+                        class="text-decoration-none">{{ $post->author->name }}</a> in <a
+                        href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
-        {!! $post->body !!}
-    </article>
+                <img src="https://random.imagecdn.app/1200/400" alt="" class="img-fluid">
 
-    <a href="/posts">Back to Posts</a>
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+
+                <a href="/posts">Back to Posts</a>
+            </div>
+        </div>
+    </div>
 @endsection
 
 {{-- Post::create([
